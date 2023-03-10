2acc07eed420bbd2985d2d017a0b003c1faa2178
Kiv0X <theolegrand7@gmail.com>
2023-03-10 16:14:23 +0000

    add db functions

--- Site.py
+++ Site.py
@@ -1,13 +1,62 @@
 from flask import Flask, render_template, jsonify, request
-import ping3
-import subprocess
+import datetime
 
+from .db_action import SemaBox, SemaBoxDB
+
+db: SemaBoxDB = SemaBoxDB("semabox.db") # LA DB
 liste_semabox = ["semabox1", "semabox2", "semabox3"]
 app = Flask(__name__)
 # default page
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+
+
+############## API ##############
+@app.route('/api/manage-semabox/add', methods=['POST'])
+def addSemabox():
+    name = request.form['name']
+    ip = request.form['ip']
+    connected = bool(request.form['connected'])
+    lastCheck = datetime.datetime.strptime(request.form['lastCheck'], "%Y-%m-%d %H:%M:%S")
+    semabox = SemaBox(
+        name=name, 
+        ip=ip, 
+        connected=connected, 
+        lastCheck=lastCheck
+    )
+    db.addSemaBox(semabox)
+    return jsonify({'result': 'success'})
+
+@app.route('/api/manage-semabox/get/<semaboxID>', methods=['GET'])
+def getSemabox(semaboxID: str):
+    semabox = db.getSemaBox(int(semaboxID))
+    return semabox.dict()
+
+
+@app.route('/api/manage-semabox/getAll', methods=['POST'])
+def getAllSemabox():
+    semaboxes = db.getAllSemaBox()
+    return [x.dict() for x in semaboxes] # Transforme la liste d'objet en liste de dictionnaire
+
+@app.route('/api/manage-semabox/update', methods=['POST'])
+def updateSemabox():
+    id = request.form['id']
+    name = request.form['name']
+    ip = request.form['ip']
+    connected = bool(request.form['connected'])
+    lastCheck = datetime.datetime.strptime(request.form['lastCheck'], "%Y-%m-%d %H:%M:%S")
+    semabox = SemaBox(
+        id=int(id),
+        name=name, 
+        ip=ip, 
+        connected=connected, 
+        lastCheck=lastCheck
+    )
+    db.updateSemaBox(semabox)
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
---
+++ db_action.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+from sqlite3worker import Sqlite3Worker
+from typing import Union 
+import datetime
+
+
+@dataclass
+class SemaBox:
+    name: str
+    ip: str
+    connected: bool
+    lastCheck: datetime.datetime
+    id: Union[int, None] = None
+
+    def dict(self) -> dict:
+        """
+        Transforme l'objet actuel en un dictionnaire JSON
+        """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ip': self.ip,
+            'connected': self.connected,
+            'lastCheck': self.lastCheck.strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+class SemaBoxDB:
+    db: Sqlite3Worker
+
+
+    def __init__(self, dbPath: str) -> None:
+        self.db = Sqlite3Worker(dbPath)
+        self.db.execute("CREATE TABLE IF NOT EXISTS semabox (id INTEGER PRIMARY KEY, name TEXT, ip TEXT, connected INTEGER, lastCheck TEXT)")
+        return 
+
+    def addSemaBox(self, semabox: SemaBox) -> None: 
+        """
+        Add a semabox to the database
+        """
+        self.db.execute("INSERT INTO semabox (name, ip, connected, lastCheck) VALUES (?, ?, ?, ?)", (semabox.name, semabox.ip, semabox.connected, semabox.lastCheck.strftime("%Y-%m-%d %H:%M:%S")))
+        return
+    
+    def getAllSemaBox(self) -> list[SemaBox]:
+        """
+        get status of all semabox
+        """
+        result = self.db.execute("SELECT * FROM semabox")
+        return [SemaBox(x[0], x[1], x[2], x[3], datetime.datetime.strptime(x[4], "%Y-%m-%d %H:%M:%S")) for x in result]
+
+    def getSemaBox(self, id: int) -> SemaBox:
+        """
+        get info about a particular semabox
+        """
+        result = self.db.execute("SELECT * FROM semabox WHERE id = ?", (id,))
+        return SemaBox(result[0][0], result[0][1], result[0][2], result[0][3], datetime.datetime.strptime(result[0][4], "%Y-%m-%d %H:%M:%S"))
+
+    def updateSemaBox(self, semabox: SemaBox) -> None:
+        """
+        update data of particular semabox
+        """
+        self.db.execute("UPDATE semabox SET name = ?, ip = ?, connected = ?, lastCheck = ? WHERE id = ?", (semabox.name, semabox.ip, semabox.connected, semabox.lastCheck.strftime("%Y-%m-%d %H:%M:%S"), semabox.id))
+        return
+
+    def getAfter15mnSemabox(self) -> list[SemaBox]:
+        """
+        get semabox that have not been checked for more than 15 minutes
+        """
+        result = self.db.execute("SELECT * FROM semabox WHERE lastCheck < datetime('now', '-15 minutes')")
+        return [SemaBox(x[0], x[1], x[2], x[3], datetime.datetime.strptime(x[4], "%Y-%m-%d %H:%M:%S")) for x in result]
